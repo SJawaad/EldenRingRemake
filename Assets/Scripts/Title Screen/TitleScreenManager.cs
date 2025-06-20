7a91a0a3c19c609996a7c3115e5253f3aea9ea93
SJawaad <jawaad.mahbub2004@gmail.com>
2025-06-20 12:11:12 +0100

    Created title screen manager and added new game button

---
+++ Assets/Scripts/Title Screen/TitleScreenManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+
+namespace JM
+{
+    public class TitleScreenManager : MonoBehaviour
+    {
+        public void StartNetworkAsHost()
+        {
+            NetworkManager.Singleton.StartHost();
+        }
+    }
+}
