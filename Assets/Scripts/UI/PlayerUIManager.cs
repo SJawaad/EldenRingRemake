ca715a92725e3db47f327f1002fbd5197771b2f6
SJawaad <jawaad.mahbub2004@gmail.com>
2025-06-27 12:04:30 +0100

    Updated player ui manager to fix netcode problem with parrelsync

--- Assets/Scripts/UI/PlayerUIManager.cs
+++ Assets/Scripts/UI/PlayerUIManager.cs
@@ -2,6 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
+using ParrelSync;
+using System;
+using UnityEditor;
+using Unity.Netcode.Transports.UTP;
 
 namespace JM
 {
@@ -11,6 +15,8 @@ namespace JM
 
         [Header("NETWORK JOIN")]
 		[SerializeField] bool startGameAsClient;
+
+        int clientNumber;
 
         private void Awake()
         {
@@ -27,18 +33,50 @@ namespace JM
         private void Start()
         {
             DontDestroyOnLoad(gameObject);
+            ChangePortForCloneEditor();
         }
 
         private void Update()
         {
-            if(startGameAsClient)
+            if (startGameAsClient)
             {
                 startGameAsClient = false;
-                // MUST FIRST SHUT DOWN NETWORK AS WE HAVE STARTED AS HOST AND WANT TO START AS CLIENT
-                NetworkManager.Singleton.Shutdown();
-                // START AS CLIENT
-                NetworkManager.Singleton.StartClient();
+                StartCoroutine(RestartAsClient());
             }
+        }
+
+        void ChangePortForCloneEditor()
+        {
+            if (!ClonesManager.IsClone())
+                return;
+
+            // convert the argument for this clone editor to the number of said editor; input checking exists below
+            // to ensure that the argument is a number, and that it starts from index 1
+            string clonesManagerArgument = ClonesManager.GetArgument();
+
+            if (!Int32.TryParse(clonesManagerArgument, out clientNumber) || clientNumber < 1)
+            {
+                Debug.Log("Change the argument of the clone editor to the number of said editor; start from 1.");
+                EditorApplication.isPlaying = false;
+            }
+
+            // finally, actually change the port
+            UnityTransport transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
+            transport.ConnectionData.Port += (ushort)clientNumber;
+        }
+
+        private IEnumerator RestartAsClient()
+        {
+            if (NetworkManager.Singleton.IsListening)
+            {
+                NetworkManager.Singleton.Shutdown();
+                UnityTransport transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
+                transport.ConnectionData.Port -= (ushort)clientNumber;
+            }
+
+            yield return null;
+
+            NetworkManager.Singleton.StartClient();
         }
     }
 }
